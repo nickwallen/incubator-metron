a758a73b11a30367e2e9527f9bf3d6a88c64e0c2
Nick Allen <nick@nickallen.org>
2018-06-28 10:50:19 -0400

    More fixes needed.  Still doesn't fix the .kibana index that gets created

--- metron-deployment/packaging/ambari/metron-mpack/src/main/resources/common-services/METRON/CURRENT/package/scripts/indexing_master.py
+++ metron-deployment/packaging/ambari/metron-mpack/src/main/resources/common-services/METRON/CURRENT/package/scripts/indexing_master.py
@@ -48,8 +48,14 @@ class Indexing(Script):
         env.set_params(params)
 
         Logger.info("Running indexing configure")
+        metron_service.check_indexer_parameters()
         File(format("{metron_config_path}/elasticsearch.properties"),
              content=Template("elasticsearch.properties.j2"),
+             owner=params.metron_user,
+             group=params.metron_group
+             )
+        File(format("{metron_config_path}/solr.properties"),
+             content=Template("solr.properties.j2"),
              owner=params.metron_user,
              group=params.metron_group
              )
@@ -91,17 +97,28 @@ class Indexing(Script):
         env.set_params(params)
         self.configure(env)
         commands = IndexingCommands(params)
+        if params.ra_indexing_writer == 'Solr':
+            # Install Solr schemas
+            try:
+                if not commands.is_solr_schema_installed():
+                    commands.solr_schema_install(env)
+                    commands.set_solr_schema_installed()
 
-        # Install elasticsearch templates
-        try:
-            if not commands.is_elasticsearch_template_installed():
-                self.elasticsearch_template_install(env)
-                commands.set_elasticsearch_template_installed()
+            except Exception as e:
+                msg = "WARNING: Solr schemas could not be installed.  " \
+                      "Is Solr running?  Will reattempt install on next start.  error={0}"
+                Logger.warning(msg.format(e))
+        else:
+            # Install elasticsearch templates
+            try:
+                if not commands.is_elasticsearch_template_installed():
+                    self.elasticsearch_template_install(env)
+                    commands.set_elasticsearch_template_installed()
 
-        except Exception as e:
-            msg = "WARNING: Elasticsearch index templates could not be installed.  " \
-                  "Is Elasticsearch running?  Will reattempt install on next start.  error={0}"
-            Logger.warning(msg.format(e))
+            except Exception as e:
+                msg = "WARNING: Elasticsearch index templates could not be installed.  " \
+                      "Is Elasticsearch running?  Will reattempt install on next start.  error={0}"
+                Logger.warning(msg.format(e))
 
         commands.start_indexing_topology(env)
 
@@ -121,6 +138,7 @@ class Indexing(Script):
     def restart(self, env):
         from params import params
         env.set_params(params)
+
         self.configure(env)
         commands = IndexingCommands(params)
         commands.restart_indexing_topology(env)
@@ -129,12 +147,14 @@ class Indexing(Script):
         from params import params
         env.set_params(params)
         Logger.info("Installing Elasticsearch index templates")
+        metron_service.check_indexer_parameters()
 
         commands = IndexingCommands(params)
         for template_name, template_path in commands.get_templates().iteritems():
 
             # install the index template
-            File(template_path, mode=0755, content=StaticFile("{0}.template".format(template_name)))
+            template_file = "{0}.template.j2".format(template_name)
+            File(template_path, mode=0755, content=Template(template_file))
             cmd = "curl -s -XPOST http://{0}/_template/{1} -d @{2}"
             Execute(
               cmd.format(params.es_http_url, template_name, template_path),
@@ -144,9 +164,11 @@ class Indexing(Script):
         from params import params
         env.set_params(params)
         Logger.info("Deleting Elasticsearch index templates")
+        metron_service.check_indexer_parameters()
 
         commands = IndexingCommands(params)
         for template_name in commands.get_templates():
+
             # delete the index template
             cmd = "curl -s -XDELETE \"http://{0}/_template/{1}\""
             Execute(
@@ -157,9 +179,9 @@ class Indexing(Script):
     def kibana_dashboard_install(self, env):
       from params import params
       env.set_params(params)
+      metron_service.check_indexer_parameters()
 
       Logger.info("Connecting to Elasticsearch on: %s" % (params.es_http_url))
-
       kibanaTemplate = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dashboard', 'kibana.template')
       if not os.path.isfile(kibanaTemplate):
         raise IOError(
@@ -184,8 +206,9 @@ class Indexing(Script):
     def zeppelin_notebook_import(self, env):
         from params import params
         env.set_params(params)
-        commands = IndexingCommands(params)
+        metron_service.check_indexer_parameters()
 
+        commands = IndexingCommands(params)
         Logger.info(ambari_format('Searching for Zeppelin Notebooks in {metron_config_zeppelin_path}'))
 
         # Check if authentication is configured on Zeppelin server, and fetch details if enabled.
