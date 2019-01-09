3da38cb84a1e732b8ea9042d17d7c256ea92fc2b
Nick Allen <nick@nickallen.org>
2019-01-09 12:03:40 -0500

    Added instructions

--- metron-analytics/metron-profiler-spark/src/main/java/org/apache/metron/profiler/spark/ProfileMeasurementAdapter.java
+++ metron-analytics/metron-profiler-spark/src/main/java/org/apache/metron/profiler/spark/ProfileMeasurementAdapter.java
@@ -28,7 +28,28 @@ import java.util.concurrent.TimeUnit;
 
 /*
 
- create table profiler1 (
+start-hbase.sh
+
+cd ~/tmp/apache-phoenix-4.14.1-HBase-1.1-bin/bin
+./queryserver.py
+
+See https://zeppelin.apache.org/docs/0.7.3/interpreter/jdbc.html#apache-phoenix
+Create Phoenix interpreter in Zeppelin using thin client.
+  Properties
+  default.driver	  org.apache.phoenix.queryserver.client.Driver
+  default.url	      jdbc:phoenix:thin:url=http://localhost:8765;serialization=PROTOBUF
+  default.user	    phoenix_user
+  default.password	phoenix_password
+
+Dependencies
+Artifact
+org.apache.phoenix:phoenix-queryserver-client:4.8.0-HBase-1.2		For Phoenix 4.8+
+
+%phoenix
+drop table profiler1;
+
+%phoenix
+create table profiler1 (
    profileName varchar not null,
    entity varchar not null,
    periodId bigint not null,
@@ -37,12 +58,33 @@ import java.util.concurrent.TimeUnit;
    constraint pk primary key (profileName, entity, periodId)
  );
 
-  spark-submit \
-    --class org.apache.metron.profiler.spark.cli.BatchProfilerCLI \
-    --properties-file ~/tmp/profiler.properties \
-    target/metron-profiler-spark-0.7.0.jar \
-    --config ~/tmp/profiler.properties \
-    --profiles ~/tmp/profiles.json
+spark-submit \
+  --class org.apache.metron.profiler.spark.cli.BatchProfilerCLI \
+  --properties-file ~/tmp/profiler.properties \
+  target/metron-profiler-spark-0.7.1.jar \
+  --config ~/tmp/profiler.properties \
+  --profiles ~/tmp/profiles.json
+
+%phoenix
+select profilename, count(*)
+from profiler1
+group by profilename;
+
+%phoenix
+select entity, count(*) as count
+from profiler1
+where profilename='hello-world-1'
+group by entity order by entity
+
+%phoenix
+select periodid, profilevalue
+from profiler1
+where profilename='hello-world-1' and entity='62.75.195.236'
+
+%phoenix
+select periodid, profilevalue
+from profiler1
+where profilename='${profile}' and entity='${entity}'
 
 */
 
